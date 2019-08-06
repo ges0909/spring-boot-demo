6cc9cdc516bc889dcc8c19565c7274c527b17af4
ges0909 <gerrit.schrader@gmail.com>
2019-08-06 07:10:56 +0200

    add legacy code to project

--- src/main/java/de/schrader/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/de/schrader/springbootdemo/SpringBootDemoApplication.java
@@ -2,9 +2,13 @@ package de.schrader.springbootdemo;
 
 import de.schrader.springbootdemo.service.PersonService;
 import de.schrader.springbootdemo.service.PersonServiceImpl;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
@@ -16,5 +20,16 @@ public class SpringBootDemoApplication {
     @Bean
     public PersonService personService() {
         return new PersonServiceImpl();
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext applicationContext) {
+        return args -> {
+            String[] beanNames = applicationContext.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String bean : beanNames) {
+                System.out.println(bean);
+            }
+        };
     }
 }
